cfda00af3b1104e0df70ecd5dfdeb86057ebbc92
bqdiu <boquangdieu2003@gmail.com>
2024-08-04 21:56:21 +0700

    main

---
+++ App/Main.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace App
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_menu = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_home = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_complaints = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btn_request = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btn_setting = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.btn_logout = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(40)))), ((int)(((byte)(45)))));
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Controls.Add(this.panel4);
+            this.flowLayoutPanel1.Controls.Add(this.panel5);
+            this.flowLayoutPanel1.Controls.Add(this.panel7);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(235, 635);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_menu);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 106);
+            this.panel1.TabIndex = 7;
+            // 
+            // btn_menu
+            // 
+            this.btn_menu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_menu.Location = new System.Drawing.Point(0, 0);
+            this.btn_menu.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_menu.Name = "btn_menu";
+            this.btn_menu.Size = new System.Drawing.Size(232, 106);
+            this.btn_menu.TabIndex = 7;
+            this.btn_menu.Text = "Menu";
+            this.btn_menu.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btn_home);
+            this.panel2.Location = new System.Drawing.Point(3, 115);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(232, 58);
+            this.panel2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(232, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Home";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_home
+            // 
+            this.btn_home.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_home.Image = ((System.Drawing.Image)(resources.GetObject("btn_home.Image")));
+            this.btn_home.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_home.Location = new System.Drawing.Point(0, 0);
+            this.btn_home.Name = "btn_home";
+            this.btn_home.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_home.Size = new System.Drawing.Size(232, 58);
+            this.btn_home.TabIndex = 1;
+            this.btn_home.Text = "Trang chủ";
+            this.btn_home.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btn_complaints);
+            this.panel3.Location = new System.Drawing.Point(3, 179);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(232, 58);
+            this.panel3.TabIndex = 8;
+            // 
+            // btn_complaints
+            // 
+            this.btn_complaints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_complaints.Image = ((System.Drawing.Image)(resources.GetObject("btn_complaints.Image")));
+            this.btn_complaints.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_complaints.Location = new System.Drawing.Point(0, 0);
+            this.btn_complaints.Name = "btn_complaints";
+            this.btn_complaints.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_complaints.Size = new System.Drawing.Size(232, 58);
+            this.btn_complaints.TabIndex = 1;
+            this.btn_complaints.Text = "Complaint";
+            this.btn_complaints.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btn_request);
+            this.panel4.Location = new System.Drawing.Point(3, 243);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(232, 58);
+            this.panel4.TabIndex = 9;
+            // 
+            // btn_request
+            // 
+            this.btn_request.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_request.Image = ((System.Drawing.Image)(resources.GetObject("btn_request.Image")));
+            this.btn_request.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_request.Location = new System.Drawing.Point(0, 0);
+            this.btn_request.Name = "btn_request";
+            this.btn_request.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_request.Size = new System.Drawing.Size(232, 58);
+            this.btn_request.TabIndex = 1;
+            this.btn_request.Text = "Request";
+            this.btn_request.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btn_setting);
+            this.panel5.Location = new System.Drawing.Point(3, 307);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(232, 58);
+            this.panel5.TabIndex = 9;
+            // 
+            // btn_setting
+            // 
+            this.btn_setting.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_setting.Image = ((System.Drawing.Image)(resources.GetObject("btn_setting.Image")));
+            this.btn_setting.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_setting.Location = new System.Drawing.Point(0, 0);
+            this.btn_setting.Name = "btn_setting";
+            this.btn_setting.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_setting.Size = new System.Drawing.Size(232, 58);
+            this.btn_setting.TabIndex = 1;
+            this.btn_setting.Text = "Setting";
+            this.btn_setting.UseVisualStyleBackColor = true;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.panel6);
+            this.panel7.Location = new System.Drawing.Point(3, 371);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(226, 267);
+            this.panel7.TabIndex = 10;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.btn_logout);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel6.Location = new System.Drawing.Point(0, 201);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(10);
+            this.panel6.Size = new System.Drawing.Size(226, 66);
+            this.panel6.TabIndex = 9;
+            // 
+            // btn_logout
+            // 
+            this.btn_logout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_logout.Image = ((System.Drawing.Image)(resources.GetObject("btn_logout.Image")));
+            this.btn_logout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_logout.Location = new System.Drawing.Point(10, 10);
+            this.btn_logout.Name = "btn_logout";
+            this.btn_logout.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_logout.Size = new System.Drawing.Size(206, 46);
+            this.btn_logout.TabIndex = 1;
+            this.btn_logout.Text = "Logout";
+            this.btn_logout.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 635);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.IsMdiContainer = true;
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_home;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btn_complaints;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btn_request;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btn_setting;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btn_logout;
+        private System.Windows.Forms.Button btn_menu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ App/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;    
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+  
+    }
+}
