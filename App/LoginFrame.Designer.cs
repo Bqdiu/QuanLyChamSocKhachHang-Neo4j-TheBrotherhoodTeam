b049eec9a40e18724a1b3b5e8030b055d5977606
tvantinh <tjnk13102003@gmail.com>
2024-08-06 11:50:52 +0700

    update login framr

--- App/LoginFrame.Designer.cs
+++ App/LoginFrame.Designer.cs
@@ -28,12 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginFrame";
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            panel1 = new System.Windows.Forms.Panel();
+            linkLabel1 = new System.Windows.Forms.LinkLabel();
+            textBox2 = new System.Windows.Forms.TextBox();
+            textBox1 = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            panel2 = new System.Windows.Forms.Panel();
+            button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources._174203378;
+            pictureBox1.Location = new System.Drawing.Point(0, 0);
+            pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(235, 214);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            label1.Location = new System.Drawing.Point(94, 14);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(234, 33);
+            label1.TabIndex = 2;
+            label1.Text = "LOGIN ACCOUNT";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            label2.Location = new System.Drawing.Point(3, 27);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(90, 23);
+            label2.TabIndex = 3;
+            label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            label3.Location = new System.Drawing.Point(3, 57);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(91, 23);
+            label3.TabIndex = 4;
+            label3.Text = "Password  :";
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Location = new System.Drawing.Point(4, 50);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(364, 121);
+            panel1.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            linkLabel1.LinkColor = System.Drawing.Color.FromArgb(224, 224, 224);
+            linkLabel1.Location = new System.Drawing.Point(254, 93);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new System.Drawing.Size(108, 14);
+            linkLabel1.TabIndex = 7;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Forgot password ?";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(105, 57);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(216, 23);
+            textBox2.TabIndex = 6;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox1.Location = new System.Drawing.Point(105, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(217, 23);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            button1.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(149, 176);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(102, 34);
+            button1.TabIndex = 6;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = System.Drawing.SystemColors.GrayText;
+            panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(panel1);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(button1);
+            panel2.Location = new System.Drawing.Point(236, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(417, 217);
+            panel2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            button2.BackColor = System.Drawing.SystemColors.HighlightText;
+            button2.Font = new System.Drawing.Font("Segoe UI Historic", 9F, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            button2.Location = new System.Drawing.Point(277, 177);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(91, 34);
+            button2.TabIndex = 8;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // LoginFrame
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.SystemColors.Window;
+            ClientSize = new System.Drawing.Size(652, 215);
+            Controls.Add(panel2);
+            Controls.Add(pictureBox1);
+            Name = "LoginFrame";
+            Text = "LoginFrame";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
     }
 }
